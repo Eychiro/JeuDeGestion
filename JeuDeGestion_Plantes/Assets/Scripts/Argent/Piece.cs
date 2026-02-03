7836a2f542133ac0aa1984ad1fbd2b2ed66bf82d
Eychiro <raphaelsymoens@yahoo.fr>
2026-02-03 23:14:19 +0100

    update friction of player and added rotation to gold coin

--- JeuDeGestion_Plantes/Assets/Scripts/Argent/Piece.cs
+++ JeuDeGestion_Plantes/Assets/Scripts/Argent/Piece.cs
@@ -4,6 +4,8 @@ public class Piece : MonoBehaviour
 {
     public int valeur;
 
+    private float vitesseRotation = 100f;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -11,5 +13,10 @@ public class Piece : MonoBehaviour
             ArgentManager.Instance.GiveMoney(valeur);
             Destroy(gameObject);
         }
+    }
+
+    void Update()
+    {
+        transform.Rotate(0f, vitesseRotation * Time.deltaTime, 0f);
     }
 }
